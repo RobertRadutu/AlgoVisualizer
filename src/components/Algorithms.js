df706f434ae5f893fd853cffc5082c92df038ef2
Robert <raduturobert20@gmail.com>
2023-03-15 20:54:59 +0200

    Interface and nodes

--- src/components/Algorithms.js
+++ src/components/Algorithms.js
@@ -1,12 +1,65 @@
 import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import { Routes } from "react-router-dom/dist";
+import Grid from "./grid/grid";
  
+const DropDown = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  const handleMenuOne = () => {
+    setOpen(false);
+  }
+
+  const handleMenuTwo = () => {
+    setOpen(false);
+  }
+
+  return (
+    <Dropdown
+      open={open}
+      trigger={<button onClick={handleOpen}>Choose the algorithm you want to visualize</button>}
+      menu={[
+        <button onClick={handleMenuOne}>Dijkstra</button>,
+        <button onClick={handleMenuTwo}>A*</button>,
+      ]}
+    />
+  );
+};
+
+const Dropdown = ({ open, trigger, menu }) => {
+  return (
+    <div className="dropdown">
+      {trigger}
+      {open ? (
+        <ul className="menu">
+          {menu.map((menuItem, index) => (
+            <li key={index} className="menu-item">{menuItem}</li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+};
+
+
 class Algorithms extends Component {
+  constructor(props){  
+    super(props);  
+  }
+
   render() {
     return (
-      <div>
-        <h2>Algorithms</h2>
-        <p>Choose your algorithm:</p>
-   
+      <div className="bodyAlgorithm">
+        <DropDown></DropDown>
+        <Grid></Grid>
       </div>
     );
   }
