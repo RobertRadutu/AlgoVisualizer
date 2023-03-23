b07734ad7533a4c35b2496e11f47105ee1dd4dab
Andra Stoica <andra.stoica123@gmail.com>
2023-03-23 10:55:14 +0200

    Added Faq.

--- src/components/Informations.js
+++ src/components/Informations.js
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
+import Faq  from "./faq/Faq";
  
 class Informations extends Component {
   render() {
     return (
       <div>
-        <h2>GOT QUESTIONS?</h2>
-        <p>The easiest thing to do is post on
-        our <a href="http://forum.kirupa.com">forums</a>.
-        </p>
+         <Faq /> 
       </div>
     );
   }
 }
  
 export default Informations;
+
